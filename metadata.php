151c1fa290144197c7b35c48a4b76a2558237021
paeddl <patrick.weinstein@patworx.de>
2026-07-14 10:02:34 +0200

    v1.0.8 - Updated Order Status Logic for Direct Bank Transfer

--- metadata.php
+++ metadata.php
@@ -16,7 +16,7 @@ $aModule = [
     ],
     'thumbnail' => 'assets/admin/unzer_payment.png',
     'lang' => 'en',
-    'version' => '1.0.7',
+    'version' => '1.0.8',
     'author' => 'Unzer GmbH',
     'email' => 'info@unzer.com',
     'url' => 'https://www.unzer.com/',
